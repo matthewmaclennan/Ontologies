2ec21606526c9f71c8e5c83b89bba7a1a6469a16
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-06 13:11:38 -0700

    Create cmoRxpath.R

---
+++ cmoRxpath.R
@@ -0,0 +1,13 @@
+#Scripts in R dealing with the RSC's Chemical Methods Ontology
+library(XML)
+library(RCurl)
+#create a vector of all chemical methods with their synonyms below
+#load the ontology into R
+CMO.owl<-htmlTreeParse(getURL("http://www.rsc.org/ontologies/CMO/CMO_OWL.owl"),useInternalNode=T)
+#obtain the main technique names
+xpathApply(CMO.owl,"//class/label")
+#obtain the vector of synonyms
+CMO_synonyms<-unlist(strsplit(unlist(xpathApply(CMO.owl,"//class/hasexactsynonym",xmlValue)),"\n +"))
+#the synonyms "hasexactsynonym" are sibling nodes to the "label" nodes (which are children nodes of "class"), 
+#so lapply() to the ancestor node ("class"), then go to the "hasexactsynonym node". Using lapply() will help to assemble the 
+#vector for searching.
