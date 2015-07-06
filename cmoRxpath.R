73e55906e209adaf648fa69f9071ad21993e6e91
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-06 13:12:29 -0700

    Update cmoRxpath.R

--- cmoRxpath.R
+++ cmoRxpath.R
@@ -6,7 +6,8 @@ library(RCurl)
 CMO.owl<-htmlTreeParse(getURL("http://www.rsc.org/ontologies/CMO/CMO_OWL.owl"),useInternalNode=T)
 #obtain the main technique names
 xpathApply(CMO.owl,"//class/label")
-#obtain the vector of synonyms
+#obtain the vector of synonyms. The unlisted xpath results contain "\n" characters with multiple spaces, so strsplit() is used
+#to clean this up.
 CMO_synonyms<-unlist(strsplit(unlist(xpathApply(CMO.owl,"//class/hasexactsynonym",xmlValue)),"\n +"))
 #the synonyms "hasexactsynonym" are sibling nodes to the "label" nodes (which are children nodes of "class"), 
 #so lapply() to the ancestor node ("class"), then go to the "hasexactsynonym node". Using lapply() will help to assemble the 
